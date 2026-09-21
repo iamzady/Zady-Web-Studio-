50b664287f2f2b22b501ad3d5e4f300be56bd769
iamzady <iamzady7@gmail.com>
2026-09-22 05:06:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/* =========================================
+   ZADY WEB STUDIO
+   COMPLETE JAVASCRIPT
+========================================= */
+
+
+/* =========================================
+   MOBILE MENU
+========================================= */
+
+const menuButton = document.getElementById("menuButton");
+const navLinks = document.getElementById("navLinks");
+
+if (menuButton && navLinks) {
+
+    menuButton.addEventListener("click", () => {
+
+        navLinks.class
